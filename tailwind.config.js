205a057c919bea437819005d560659bd1397ada3
Erenriquezp <erenriquezp@uce.edu.ec>
2025-01-11 17:28:12 -0500

    refactor layout and styles: update main layout structure, enhance TailwindCSS configuration, and define global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{html,ts}', // Asegura que Tailwind procese todos los archivos Angular
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#d3435b',
+        secondary: '#721652',
+        button: '#420054',
+        footer: '#420054',
+      },
+    },
   },
   plugins: [],
 }
